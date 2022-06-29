2bc95d5e8194eb137831d94a55d5fdee7fc30c9f
Balmeet_Singh <balmeetsingh44@gmail.com>
2022-06-29 12:25:56 +0530

    add

---
+++ src/Redux/Actions/index.js
@@ -0,0 +1,11 @@
+const incNum=()=>{
+    return {
+        type:"INC"
+    }
+}
+const decNum=()=>{
+    return {
+        type:"DEC"
+    }
+}
+export {incNum,decNum}
---
+++ src/Redux/Reducers/index.js
@@ -0,0 +1,6 @@
+import changeTheNumber from "./numChange";
+import { combineReducers } from "redux";
+const rootReducer=combineReducers({
+    changeTheNumber
+})
+export default rootReducer
